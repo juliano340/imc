f3847138ec13df715e947d2c26042bf43dd0d447
juliano340 <juliano340@gmail.com>
2022-07-02 08:12:56 -0300

    Adicionado useState peso e altura.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import "./app.css";
+import { useState } from "react";
+
+
 export default function App () {
+
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+  
+
   return (
     <div className="app">
       <h1>Calculadora IMC</h1>
       <span>Vamos calcular seu IMC</span>
       <div className="area-input">
         
-        <input type="text" placeholder="Peso em (kg) Ex: 80"/>
-        <input type="text" placeholder="Altura em (cm) Ex: 180"/>
+        <input 
+        type="text" 
+        placeholder="Peso em (kg) Ex: 80" 
+        value={peso}
+        onChange={(e) => setPeso(e.target.value)}
+        />
+        <input type="text" 
+        placeholder="Altura em (cm) Ex: 180" 
+        value={altura}
+        onChange={(e) => setAltura(e.target.value)}
+        />
         <button>Calcular</button>
 
       </div>
